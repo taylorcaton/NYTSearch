6e3a5f4cdd47cf14fcaffd615f08e9a8c623df88
Taylor Caton <taylorcaton@gmail.com>
2017-07-06 20:30:58 -0400

    started js

---
+++ assets/js/js.js
@@ -0,0 +1,47 @@
+function getArticle(searchTerm, beginYear, endYear){
+
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += '?' + $.param({
+	  'api-key': "dd83170674244b5fa23b6f8784ecbf99",
+	  'q': searchTerm,
+	  'begin_date': beginYear + "0101",
+	  'end_date': endYear + "0101"
+	});
+	$.ajax({
+	  url: url,
+	  method: 'GET',
+	}).done(function(result) {
+	  console.log(result);
+
+	  updateScreen(results)
+
+	}).fail(function(err) {
+	  throw err;
+	});
+
+}
+
+$(document).on("click", "#submit", function(){
+
+});
+
+function updateScreen(data){
+
+	var headline;
+	var summary; 
+	var link;
+	var pubDate;
+
+	for (var i = 0; i < data.docs.length; i++) {
+		link = data.docs[i].web_url;
+		summary = data.docs[i].snippet;
+		headline = data.docs[i].headline.main;
+		pubDate = data.docs[i].pub_date;
+
+
+		$("#link").text(link);
+		$("#summary").text(summary);
+		$("#headline").text(headline);
+		$("#pubDate").text(pubDate);
+	}
+}
